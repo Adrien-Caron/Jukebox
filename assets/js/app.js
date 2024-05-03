7e7dec4410535b33f4eed78bc0f160a77824c862
Adrien Caron <adrien62360@outlook.fr>
2024-05-03 16:16:54 +0200

    three commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ const config = {
 };
 
 const getData = async () =>{
-    const req = await fetch("https://api-good.onrender.com/api/V1/musics");
+    const req = await fetch("https://api-final-htv1.onrender.com/api/V1/musics");
     const dbMusic = await req.json();
     data = dbMusic.result ; 
     console.log("result ", dbMusic);
